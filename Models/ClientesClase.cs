9f51ff603956fa77dbeaea78d3bf1db015d89f83
Juan Andres Alvarez Pullas <juan.alvarez.pullas@udla.edu.ec>
2023-12-17 10:16:23 -0500

    Modificación de atributos

--- Models/ClientesClase.cs
+++ Models/ClientesClase.cs
@@ -12,7 +12,7 @@ namespace ProyectoP2.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         [StringLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres.")]
         public string Nombre { get; set; }
 
